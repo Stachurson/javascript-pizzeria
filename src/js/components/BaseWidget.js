d0ed5d9fa8478179c2c8c0cf088777a3a92b2cfe
Mateusz Stachurski <mateusz.stachurski@icloud.com>
2020-07-06 20:50:29 +0200

    Add HourPicker

--- src/js/components/BaseWidget.js
+++ src/js/components/BaseWidget.js
@@ -1,57 +1,51 @@
 export class BaseWidget {
+  constructor(wrapperElement, initialValue) {
+    const thisWidget = this;
 
-    constructor(wrapperElement, initialValue) {
-        const thisWidget = this;
-        thisWidget.dom = {};
-        thisWidget.dom.wrapper = wrapperElement;
-        thisWidget.correctValue = initialValue;
-    };
+    thisWidget.dom = {}; //tworzymy obiekt
+    thisWidget.dom.wrapper = wrapperElement; // zapisujemy w nim właściwosć wrapper której wartością ma byc argument wrapperElement
+    thisWidget.correctValue = initialValue; // we własciwosci thisWidget.correctValue zapisujemy wartosć argumentu initialValue
+  }
 
-    get value() {
+  get value(){ // uruchomi się przy próbie odczytania wartości
+    const thisWidget = this;
 
-        const thisWidget = this;
+    return thisWidget.correctValue;
+  }
 
-        return thisWidget.correctValue;
-    };
+  set value(assignedValue){ // uruchomi sie przy próbie zmiany wartości value
+    const thisWidget = this;
 
-    set value(assignedValue) {
+    const newValue = thisWidget.parseValue(assignedValue); // parseValue ma konwertować liczbę z tekstu wpisanego w inpucie na liczbę
 
-        const thisWidget = this;
-        const newValue = thisWidget.parseValue(assignedValue);
+    if(newValue != thisWidget.correctValue && thisWidget.isValid(newValue)){
+      thisWidget.correctValue = newValue;
+      thisWidget.announce();
+    }
+    thisWidget.renderValue();
+  }
 
-        if (newValue != thisWidget.correctValue && thisWidget.isValid(newValue)) {
-            thisWidget.correctValue = newValue;
-            thisWidget.announce();
-        };
+  parseValue(newValue){ // jeśli jej się nie uda zwraca NaN
+    return parseInt(newValue);
+  }
 
-        thisWidget.renderValue();
+  isValid(newValue){
+    return !isNaN(newValue); // prawda oznacza to nie jest NaN
+  }
 
-    };
+  renderValue(){
+    const thisWidget = this;
 
-    parseValue(newValue) {
-        return parseInt(newValue);
-    };
+    console.log('widget value:', thisWidget.value);
+  }
 
-    isValid(newValue) {
-        return !isNaN(newValue);
-    };
+  announce() {
+    const thisWidget = this;
 
-    renderValue() {
-        const thisWidget = this;
+    const event = new CustomEvent('updated', {
+      bubbles: true // dzieki właściwości bubles którą włączyliśmy po wykonaniu eventu na jakimś elemencie będzie on przekazywany rodzicowi i rodzicowi rodzica aż do body, document i window
+    });
+    thisWidget.dom.wrapper.dispatchEvent(event); // dom.wrapper to element na którym wywołujemy event
+  }
 
-        console.log('widget value:', thisWidget.value);
-    };
-
-    announce() {
-        const thisWidget = this;
-
-        const event = new CustomEvent('updated', {
-            bubbles: true
-        });
-
-        thisWidget.dom.wrapper.dispatchEvent(event);
-    };
-
-};
-
-export default BaseWidget;
+}
