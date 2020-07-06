1306c34c4e4e32bbfcec26e6c3692581d62e672a
Mateusz Stachurski <mateusz.stachurski@icloud.com>
2020-07-06 17:04:22 +0200

    /FINISHED/ Add subpages

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,19 @@
-import {
-  settings,
-  select,
-  classNames,
-  templates
-} from './settings.js';
+
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
-  initPages: function () {
+
+  initPages: function(){
+
     const thisApp = this;
-    /*wszystkie kontenery podstron (section.id=booking i order) ktore wyszukiwane sa w drzewie DOM*/
-    thisApp.pages = document.querySelector(select.containerOf.pages).children;
-    thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
-    const idFromHash = window.location.hash.replace('#/', '');
+    thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
 
-    let pageMatchingHash = thisApp.pages[0].id;
+    thisApp.navLinks = Array.from(document.querySelectorAll(select.nav.links));
 
-    for (let page of thisApp.pages) {
-      if (page.id == idFromHash) {
-        pageMatchingHash = page.id;
-        break;
-      }
-    }
-    thisApp.activatePage(pageMatchingHash);
+    thisApp.activatePage(thisApp.pages[0].id);
 
     for (let link of thisApp.navLinks) {
       link.addEventListener('click', function (event) {
@@ -33,28 +22,28 @@ const app = {
 
         /* get page id from href attribute */
         const id = clickedElement.getAttribute('href').replace('#', '');
+
         /* run thisApp.activatePage with that id */
         thisApp.activatePage(id);
-        /* change URL hash */
-        window.location.hash = '#/' + id;
+
       });
     }
-
   },
+
   activatePage: function (pageId) {
+
     const thisApp = this;
-    /* add class "active" to matching pages, remove from non-matching */
+
     for (let page of thisApp.pages) {
       page.classList.toggle(classNames.pages.active, page.id == pageId);
     }
-    /* add class "active" to matching links, remove from non-matching */
+
     for (let link of thisApp.navLinks) {
-      link.classList.toggle(
-        classNames.nav.active,
-        link.getAttribute('href') == '#' + pageId
-      );
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
     }
+
   },
+
   initMenu: function () {
     const thisApp = this;
     // console.log('thisApp.data', thisApp.data);
@@ -87,16 +76,18 @@ const app = {
 
   init: function () {
     const thisApp = this;
-    console.log('*** App starting ***');
-    console.log('thisApp:', thisApp);
-    console.log('classNames:', classNames);
-    console.log('settings:', settings);
-    console.log('templates:', templates);
+    // console.log('*** App starting ***');
+    // console.log('thisApp:', thisApp);
+    // console.log('classNames:', classNames);
+    // console.log('settings:', settings);
+    // console.log('templates:', templates);
 
     thisApp.initPages();
     thisApp.initData();
+
     thisApp.initCart();
   },
+
   initCart: function () {
     const thisApp = this;
 
@@ -104,9 +95,11 @@ const app = {
     thisApp.cart = new Cart(cartElem);
 
     thisApp.productList = document.querySelector(select.containerOf.menu);
-    thisApp.productList.addEventListener('add-to-cart', function (event) {
+
+    thisApp.productList.addEventListener('add-to-cart', function(event){
       app.cart.add(event.detail.product);
     });
+
   }
 };
 
